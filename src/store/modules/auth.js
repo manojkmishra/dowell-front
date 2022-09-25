60b741a1164ca5c65ecfb65ad112d54dba3f32ca
manojkmishra <manojmishra571980@gmail.com>
2022-09-25 17:11:09 +1000

    mock data added

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -22,15 +22,19 @@ export default{
   actions: { //actions take first option as dispatch(action) or commit(mutation)
       async signIn({dispatch},formdata){ 
         let resp= await axios.post('/auth/signin',formdata); // eslint-disable-line
-      
+      console.log('mock-auth.js-resp=',resp)
         return dispatch('attempt',resp.data.token);
         },
       async attempt({commit, state},token1){ //to validate the token of this user
+
+        console.log('mock-toekn=',token1)
+        //token1="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9hdXRoXC9zaWduaW4iLCJpYXQiOjE2NjM2MzA2MDUsImV4cCI6MTY2MzY3MDIwNSwibmJmIjoxNjYzNjMwNjA1LCJqdGkiOiJ4Znh3SGdEV2FXUjNSSGJzIiwic3ViIjo0NSwicHJ2IjoiYzA1ZjUzZjA0Yjc2MTJiNjUwYmY2MjA1MGY4NDA5NWRhNjk2NDliMyJ9.2Ntp9tRDseh46leZAuy6R5v6D_7k5SgcesEknRLc5rY"
           if(token1) commit('SET_TOKEN',token1) //dont SET_TOKEN if no token
           if(!state.token) return //dont fire me api if no token
           try{ let resp=await axios.get('auth/me'//,{headers:{'Authorization':'Bearer'+token1}}
                                                )
-             
+            //resp={"email":"manoj.mishra@dowell.com.au","name":"Manoj Mishra","admin":"1","location_id":"2"}
+             console.log('mock-attempt-resp',resp)
                commit('SET_USER',resp.data)
                }catch(e)
                     {
